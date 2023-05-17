d5f524bd1d0e55cd41adfdf09b9703a19a6f93c1
Heclo <knudski@gmail.com>
2023-05-17 19:35:42 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -210,7 +210,7 @@ uint32_t MINIMUM_RPM_SPEED_CONTROL = 200;
 
  //assign speed control PID values values are x10000
  fastPID speedPid = {      //commutation speed loop time
- 		.Kp = 5,
+ 		.Kp = 20,
  		.Ki = 1,
  		.Kd = 10,
  		.integral_limit = 10000,
