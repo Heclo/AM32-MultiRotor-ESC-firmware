adfa996793071b85a14a9efaf8098a191eaaf289
Heclo <knudski@gmail.com>
2024-02-12 15:59:41 +0100

    Update mtcu.c

--- Src/mtcu.c
+++ Src/mtcu.c
@@ -223,7 +223,7 @@ void setChannelsMTCU()
       }
        if (input_line[3] != 0)
       {
-        playBlueJayTune();
+        playInputTune();
       }
         uint8_t check = (input_line[0] + input_line[1] + input_line[2]) & 0x7F;
         if (input_line[3] == check)
