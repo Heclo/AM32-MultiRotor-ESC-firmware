6dcd4b35c789ffb9f66cf2f75a3a3a6fa7723bd4
Heclo <knudski@gmail.com>
2024-02-13 21:06:47 +0100

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -223,7 +223,7 @@
 
 uint8_t drive_by_rpm = 1;
 uint32_t MAXIMUM_RPM_SPEED_CONTROL = 10800;
-uint32_t MINIMUM_RPM_SPEED_CONTROL = 100;
+uint32_t MINIMUM_RPM_SPEED_CONTROL = 120;
 
  //assign speed control PID values values are x10000
  fastPID speedPid = {      //commutation speed loop time
